4f8275c0a0e616d4206629691a5ef7c0b99bf89c
Nikki Thomas <thom.nikki@gmail.com>
2015-03-24 10:35:32 -0400

    tried to fix dist on heroku

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 5000, function () {
-  app.use(express.static('dist'));
+  app.use('/', express.static('dist'));
 });
